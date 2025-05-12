95f3527b3c15c2e28c15395357207bd630b6e52b
Amy Wen <wen.amy@gmail.com>
2025-05-12 09:29:58 -0400

    update namespace and add background location package

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.pandaboat"
+    namespace = "com.pandateam.pandaBoat"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -20,10 +20,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.pandaboat"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        applicationId = "com.pandateam.pandaBoat"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
--- android/app/src/main/kotlin/com/example/pandaboat/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/pandaboat/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.pandaboat
+package com.pandateam.pandaBoat
 
 import io.flutter.embedding.android.FlutterActivity
 
